0b3f63c2d0f09a8c1e4b4859e6d5d92ded0b3407
AbhishekPal-1999 <abhishek@lionobytes.com>
2025-05-08 11:22:40 +0530

    Abhishek Latest Updated Code & New Code

---
+++ src/test/java/com/liono360/leads/TC_0010ActivityLogsWorkInboundCall.java
@@ -0,0 +1,99 @@
+package com.liono360.leads;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import com.liono360.loginPage.Baseclass;
+import com.liono360.pageObject.LeadPage;
+import com.liono360.pageObject.LoginPage;
+import com.liono360.utility.ExcelDataProvider;
+
+public class TC_0010ActivityLogsWorkInboundCall extends Baseclass{
+	
+	@Test
+	public void TC0010_ActivityLogsWorkInboundCall() throws InterruptedException, IOException {
+		//redirectToCRM();
+
+		LeadPage leads = new LeadPage(driver);
+		ExcelDataProvider exceldata=new ExcelDataProvider();
+		
+		waitUntilPageLoad();
+		Thread.sleep(2000);
+		
+		TryClick(leads.clickLead());
+		logger.info("Clicked on Lead ");
+
+		waitUntilPageLoad();
+		Thread.sleep(5000);
+		
+		TryClick(leads.listedLeads());
+		logger.info("clicked on view option");
+		
+	   
+
+// Activity Work Inbounds Call====================================================================================================		
+		TryClick(leads.Activity_Lead());
+		logger.info("clicked on Activity");
+		
+		TryClick(leads.Add_Logs());
+		logger.info("clicked on Add log button");
+		
+		TryClick(leads.Log_Types());
+		Thread.sleep(1000);
+		TryClick(leads.Mobiles());
+		logger.info("Work Log type Selected");
+		
+		
+		Thread.sleep(2000);
+		logger.info("Selected Logs SubType is "+leads.SubTypes().getText());
+		
+		leads.Add_Comments().sendKeys("This comment represent the Work");
+		logger.info("Comment is added under Work log type");
+		
+		TryClick(leads.clickSave());
+		logger.info("Clicked on Save button");
+
+// Edit Work Inbounds Call========================================================================================================
+
+		waitUntilPageLoad();
+		Thread.sleep(1000);
+				
+		TryClick(leads.Click_Activity());
+		logger.info("Clicked on Activity Record");
+				
+		TryClick(leads.Edit_Bttn());
+		logger.info("Clicked on Edit button");
+				
+		leads.Add_Comments().clear();
+		leads.Add_Comments().sendKeys("This comment represent the Updated Work");
+		logger.info("Comment is Updated under Mobile log type");
+				
+		leads.Edit_Add_Comments().sendKeys("This comment represent the new comment while edit Work");
+		logger.info("New Comment is added under for Work");
+				
+		TryClick(leads.SaveAllDetails());
+		logger.info("Clicked on Save button");
+
+//Delete Work Inbounds Call============================================================================================
+		waitUntilPageLoad();
+		Thread.sleep(1000);
+				
+		TryClick(leads.Click_Activity());
+		logger.info("Clicked on Activity Record");
+				
+		TryClick(leads.DeleteLog());
+		logger.info("Clicked on Delete button");
+				
+		TryClick(leads.btnYes());
+		logger.info("Clicked on yes button to delete the log");
+						
+		
+	}
+	
+
+}
